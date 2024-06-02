7fa7d1e2c589b8d4df213e003bddfd5d9200c59e
jrpbjr <jrpbjr@gmail.com>
2024-06-02 20:45:53 -0300

    first commit

---
+++ src/main/java/com/jrpbjr/jrpbjrsecurity/JrpbjrSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.jrpbjrsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrpbjrSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrpbjrSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jrpbjr/jrpbjrsecurity/JrpbjrSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.jrpbjrsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrpbjrSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
