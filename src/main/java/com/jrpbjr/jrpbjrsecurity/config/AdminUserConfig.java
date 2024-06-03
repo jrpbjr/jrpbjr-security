85e41778bb547ebde52e737d3de9d53995a5b6ba
jrpbjr <jrpbjr@gmail.com>
2024-06-02 22:47:51 -0300

    adicionando o controller do token

---
+++ src/main/java/com/jrpbjr/jrpbjrsecurity/config/AdminUserConfig.java
@@ -0,0 +1,2 @@
+package com.jrpbjr.jrpbjrsecurity.config;public class AdminUserConfig {
+}
--- src/main/java/com/jrpbjr/jrpbjrsecurity/controller/TokenController.java
+++ src/main/java/com/jrpbjr/jrpbjrsecurity/controller/TokenController.java
@@ -1,2 +1,67 @@
-package com.jrpbjr.jrpbjrsecurity.controller;public class TokenController {
+package com.jrpbjr.jrpbjrsecurity.controller;
+
+import com.jrpbjr.jrpbjrsecurity.controller.dto.LoginRequest;
+import com.jrpbjr.jrpbjrsecurity.controller.dto.LoginResponse;
+import com.jrpbjr.jrpbjrsecurity.repository.UserRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jrpbjr.jrpbjrsecurity.entities.Role;
+
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestController
+public class TokenController {
+
+    private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public TokenController(JwtEncoder jwtEncoder,
+                           UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+
+        var user = userRepository.findByUsername(loginRequest.username());
+
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)) {
+            throw new BadCredentialsException("user or password is invalid!");
+        }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+
+        var scopes = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("mybackend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
+    }
 }
